221fa4a220121c620502a6e87274d91c876935c6
Andrei Mihai <andreimihai2305is@gmail.com>
2024-12-22 11:32:48 +0200

    added notion of string_view

--- string_builder.c
+++ string_builder.c
@@ -2,10 +2,10 @@
 
 bool sb_init_builder(String_Builder* sb)
 {
-    sb->items = (char*) malloc(sizeof(char)*DEFAULT_SIZE);
+    sb->items = malloc(sizeof(char) * DEFAULT_SIZE);
     if (sb->items == NULL)
     {
-        fprintf(stderr, "[ERROR]: Could not allocate memory for 'sb_builder'\n");
+        fprintf(stderr, "[ERROR]: Could not allocate memory for 'String_Builder'\n");
         return 1;
     }
 
@@ -14,11 +14,35 @@ bool sb_init_builder(String_Builder* sb)
     return 0;
 }
 
+static void resize(String_Builder* sb)
+{
+    sb->items = realloc(sb, sb->size*2);
+    if (sb->items == NULL)
+    {
+        fprintf(stderr, "[ERRORR]: Could not allocate memory for 'String_Builder'\n");
+        exit(1);
+    }
+    sb->size = sb->size * 2;
+}
+
 void sb_append_char(String_Builder* sb, const char* c)
 {
-    if (sb->len < sb->size)
+    if (sb->len == sb->size)
     { 
-        sb->items[sb->len] = c[0];
-        sb->len++;
+        resize(sb);
+    }
+    sb->items[sb->len] = c[0];
+    sb->len++;
+}
+
+void sb_to_sv(String_Builder* sb, String_View* sv)
+{
+    if (sb == NULL || sv == NULL) return;
+    sv->len = sb->len;
+    sv->items = malloc(sizeof(char) * sv->len);
+    for (int i = 0; i < sv->len; ++i)
+    {
+        sv->items[i] = sb->items[i];
     }
 }
+
--- string_builder.h
+++ string_builder.h
@@ -14,8 +14,13 @@ typedef struct {
 
 } String_Builder;
 
+typedef struct {
+    char* items;
+    int len;
+} String_View;
+
 bool sb_init_builder(String_Builder* sb);
 void sb_append_char(String_Builder* sb, const char* c);
-
+void sb_to_sv(String_Builder* sb, String_View* sv);
 
 #endif // STRING_BUILDER_H_
